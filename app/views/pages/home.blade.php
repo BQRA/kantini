8563c30a3f6c780049df78a434f87159053b31ef
Baran Şengül <baransengul@outlook.com>
2014-07-24 16:03:29 +0300

    Revert "views dosyaları çalışmaları"
    This reverts commit 24e21d6e5077460bcf0f0797918a4a0d14b7d613.

--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -9,25 +9,16 @@
 	@include('layouts.send-post')
 
 	@foreach($posts as $post)
-		
-		@if($post->type == '0')
-			<p>
-				{{ $post->post }} | <a href="{{ URL::action('show.post', $post->id) }}">Yorum yaz</a> <br>
-				{{ $post->gender }} | 
-				
-				@if($post->member == 1)
-					<a href="{{ URL::action('show.profile', $post->username) }}">{{ $post->username }}</a> |
-				@else 
-					{{ $post->username }} |
-				@endif
-				{{ $post->created_at }}
-
-				{{ $post->type }}
-			</p>
-		@endif
-
-		@if($post->type == '1')
-			{{ 'Etkinlik' }} | <a href="{{ URL::action('show.organization', $post->id) }}">Yorum yaz</a>
-		@endif
+		<p>
+			{{ $post->post }} | <a href="{{ URL::action('show.post', $post->id) }}">Yorum yaz</a> <br>
+			{{ $post->gender }} | 
+			
+			@if($post->member == 1)
+				<a href="{{ URL::action('show.profile', $post->username) }}">{{ $post->username }}</a> |
+			@else 
+				{{ $post->username }} |
+			@endif
+			{{ $post->created_at }}
+		</p>
 	@endforeach
 @stop
