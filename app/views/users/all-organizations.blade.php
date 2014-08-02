cfd3605b3706b183f6e2aecca3440a7a4eda0d20
Baran Şengül <baransengul@outlook.com>
2014-08-03 01:04:32 +0300

    fixed #35
    usersAllOrganization sayfasi kaldirildi

--- app/views/users/all-organizations.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.master')
-
-@section('content')
-	<p><a href="{{ URL::action('show.profile', $user->username) }}">{{ $user->username }}</a> kullanıcısının tüm gönderileri</p>
-
-	@foreach($orgs_all as $orgs)
-		<li>{{ $orgs->org_name }} => <a href="{{ URL::action('show.organization', $orgs->id) }}">Oku</a></li>
-	@endforeach
-@stop
